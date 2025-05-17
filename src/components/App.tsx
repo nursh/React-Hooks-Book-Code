d007396d7de533221459bc246783a7193323c3b4
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-17 15:22:35 -0400

    Add router to app

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router"
+
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" />
+    </Routes>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+import App from './components/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
