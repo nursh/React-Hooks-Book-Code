a7c77612a47876dce980685c7512023a7dbc7fd5
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-18 16:45:54 -0400

    Add subdirectories to components directory

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router"
+import "../App.css";
+
 
 
 function App() {
---
+++ src/components/Users/UserPIcker.tsx
@@ -0,0 +1,7 @@
+export default function UserPicker() {
+  return (
+    <select name="" id="">
+      <option value="">Users</option>
+    </select>
+  )
+}
