a3ef02b5b5b5034ca17789fbe7b4f1e30282ecf2
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-18 17:03:46 -0400

    Add base routes for app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,48 @@
-import { Routes, Route } from "react-router"
+import { Routes, Route, Link } from "react-router";
+import BookablesPage from "./Bookables/BookablesPage";
+import BookingsPage from "./Bookings/BookingsPage";
+import UserPicker from "./Users/UserPIcker";
+import UsersPage from "./Users/UsersPage";
+
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from "react-icons/fa";
 import "../App.css";
 
-
-
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" />
-    </Routes>
-  )
+    <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/bookings" className="btn btn-header">
+                <FaCalendarAlt />
+                <span>Bookings</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/bookables" className="btn btn-header">
+                <FaDoorOpen />
+                <span>Bookables</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/users" className="btn btn-header">
+                <FaUsers />
+                <span>Users</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <UserPicker />
+      </header>
+
+      <Routes>
+        <Route path="bookings" element={<BookingsPage />} />
+        <Route path="bookables" element={<BookablesPage />} />
+        <Route path="users" element={<UsersPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
