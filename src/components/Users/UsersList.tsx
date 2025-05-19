a650f8ae0effcade524919db3ed9d0b4eb2d5a13
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-19 14:13:35 -0400

    update userslist to show user details

--- src/components/Users/UsersList.tsx
+++ src/components/Users/UsersList.tsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
-import { users } from '../../static.json';
-
+import { users } from "../../static.json";
 
 export default function UsersList() {
   const [currentUser, setCurrentUser] = useState(0);
 
-
   return (
-    <ul className="items-list-nav">
-      {
-        users.map((user, idx) => (
-          <li key={user.id} className={idx === currentUser ? "selected" : undefined}>
-            <button className="btn" onClick={() => setCurrentUser(idx)}>{user.name}</button>
+    <>
+      <ul className="items-list-nav">
+        {users.map((user, idx) => (
+          <li
+            key={user.id}
+            className={idx === currentUser ? "selected" : undefined}
+          >
+            <button className="btn" onClick={() => setCurrentUser(idx)}>
+              {user.name}
+            </button>
           </li>
-        ))
+        ))}
+      </ul>
+      {
+        <div className="item">
+          <div className="item-header">
+            <h2>{users[currentUser].name}</h2>
+          </div>
+          <div className="item-details">
+            <h3>{users[currentUser].title}</h3>
+            <p>{users[currentUser].notes}</p>
+          </div>
+        </div>
       }
-    </ul>
-  )
+    </>
+  );
 }
