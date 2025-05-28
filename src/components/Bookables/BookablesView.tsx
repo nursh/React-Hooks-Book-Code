5a6214718da724ba0548e5d69724c62fb90eb5ed
nur-sh <nsheikhmohammed01@gmail.com>
2025-05-28 16:59:49 -0400

    using useCallback function

--- src/components/Bookables/BookablesView.tsx
+++ src/components/Bookables/BookablesView.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import BookablesList from "./BookablesList";
 import BookableDetails from "./BookableDetails";
 import { type Bookable } from "../../types";
@@ -6,10 +6,17 @@ import { type Bookable } from "../../types";
 
 export default function BookablesView() {
   const [bookable, setBookable] = useState<Bookable>();
+  
+  const updateBookable = useCallback((b: Bookable) => {
+    if (b) {
+      console.log(b.title);
+      setBookable(b);
+    }
+  }, []);
 
   return (
     <>
-      <BookablesList bookable={bookable} setBookable={setBookable} />
+      <BookablesList bookable={bookable} setBookable={updateBookable} />
       <BookableDetails bookable={bookable} />
     </>
   )
